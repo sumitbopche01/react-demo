ec12b830ade47664a481a55633742c57c68c6bd0
codeitdown <sumitbopche01@gmail.com>
2020-12-21 13:04:34 +0530

    routing for different pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SigninPage from "./pages/signin";
+import SignupPage from "./pages/signup";
+import HomePage from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={SigninPage} exact />
+        <Route path="/home" component={HomePage} exact />
+        <Route path="/signup" component={SignupPage} exact />
+      </Switch>
+    </Router>
   );
 }
 
